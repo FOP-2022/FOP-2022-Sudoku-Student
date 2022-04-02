b4e454cbb402ed0f05290770bc515fff76adf9a8
Alexander Staeding <alexanderstaeding@gmail.com>
2022-04-02 13:43:51 +0200

    Add more javadoc to RandomGridGenerator

--- src/main/java/sudoku/RandomGridGenerator.java
+++ src/main/java/sudoku/RandomGridGenerator.java
@@ -4,8 +4,23 @@ public class RandomGridGenerator implements GridGenerator {
 
     private final SudokuSolver solver;
 
+    /**
+     * Range [0, 1].
+     */
     private final double density;
 
+    /**
+     * Creates a new random grid generator.
+     *
+     * <p>
+     * The density is in range [0, 1]. A density of 1 means that all original
+     * solved cells are kept. A density of 0 means that no original cells are
+     * kept (all cells would be 0).
+     * </p>
+     *
+     * @param solver The solver to use to generate a solved grid
+     * @param density The density of original solved cells to keep in range [0, 1].
+     */
     public RandomGridGenerator(final SudokuSolver solver, final double density) {
         this.solver = solver;
         this.density = density;
